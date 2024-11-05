4e86e6775d21783e7ef54857a99aefa167044109
Hannah <hannahmukhtaar@gmail.com>
2024-11-04 23:58:51 -0800

    Update index.js

--- index.js
+++ index.js
@@ -6,12 +6,10 @@ app.use(express.json());
 
 let todos = [];
 
-// Endpoint to retrieve all todos
 app.get('/todos', (req, res) => {
   res.json(todos);
 });
 
-// Endpoint to add a new todo
 app.post('/todos', (req, res) => {
   const { task } = req.body;
   const newTodo = { id: todos.length + 1, task };
@@ -19,7 +17,6 @@ app.post('/todos', (req, res) => {
   res.status(201).json(newTodo);
 });
 
-// Endpoint to update a todo
 app.put('/todos/:id', (req, res) => {
   const { id } = req.params;
   const { task } = req.body;
@@ -33,14 +30,12 @@ app.put('/todos/:id', (req, res) => {
   }
 });
 
-// Endpoint to delete a todo
 app.delete('/todos/:id', (req, res) => {
   const { id } = req.params;
   todos = todos.filter((t) => t.id !== parseInt(id));
   res.status(204).send();
 });
 
-// Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
